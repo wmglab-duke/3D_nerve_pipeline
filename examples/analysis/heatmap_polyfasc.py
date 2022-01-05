196a4306a1bd0536a76d957030c669cc9c3a34c8
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-01-05 13:13:37 -0500

    revert heatmap

--- examples/analysis/heatmap_polyfasc.py
+++ examples/analysis/heatmap_polyfasc.py
@@ -19,30 +19,25 @@ import sys
 
 sys.path.append(os.path.sep.join([os.getcwd(), '']))
 
-os.chdir('D:/ascent/fresh')
-
 import matplotlib.pyplot as plt
 from src.core.query import Query
 
 # set default fig size
 plt.rcParams['figure.figsize'] = [16.8/3, 10.14*2 * 0.9]
-fig,axs = plt.subplots(3,2,figsize=(25, 25))
 
-samples = [3070,3150,3230]
-for i in range(3):
-    # initialize and run Querys
-    q = Query({
-        'partial_matches': True,
-        'include_downstream': True,
-        'indices': {
-            'sample': samples[i],
-            'model': [0],
-            'sim': [3000]
-        }
-    }).run()
-    
-    # NOTE: these values were copied from the output of heatmaps(), setting the track_colormap_bounds flag True
-    colormap_bounds_override = None
+# initialize and run Querys
+q = Query({
+    'partial_matches': True,
+    'include_downstream': True,
+    'indices': {
+        'sample': [3006, 3007],
+        'model': [0, 1, 2, 3],
+        'sim': [3000]
+    }
+}).run()
+
+# NOTE: these values were copied from the output of heatmaps(), setting the track_colormap_bounds flag True
+colormap_bounds_override = None
 # colormap_bounds_override = [
 #     [
 #         (0.322187, 1.51728),
@@ -114,28 +109,21 @@ for i in range(3):
 # ]
 
 # builds heatmaps
-
-
-    q.heatmaps(plot=False,
-                # save_path='out/analysis',
-                plot_mode='fibers',
-            #    rows_override=6,
-               colorbar_aspect=5,
-               colormap_str='viridis',
-               tick_count=4,
-               reverse_colormap=True,
-                title_toggle=False,
-                track_colormap_bounds=True,
-            #    track_colormap_bounds_offset_ratio=0.0,
-                colomap_bounds_override=[[2.9,5.6],[2.9,5.6]],
-            #    subplot_title_toggle=False,
-                colorbar_text_size_override=30,
-                add_colorbar = False,
-                comp_sim = True,
-                override_axes =axs[i],
-                dotsize = 15,
-                show_orientation_point = True
-            #    tick_bounds=True
+q.heatmaps(plot=False,
+            save_path='out/analysis',
+            plot_mode='fiber0',
+        #    rows_override=6,
+           colorbar_aspect=5,
+           colormap_str='viridis',
+           tick_count=4,
+           reverse_colormap=True,
+        #    title_toggle=False,
+        #    track_colormap_bounds=True,
+        #    track_colormap_bounds_offset_ratio=0.0,
+        #    colomap_bounds_override=colormap_bounds_override,
+        #    subplot_title_toggle=False,
+            colorbar_text_size_override=30
+        #    tick_bounds=True
            )
 
 #
@@ -143,45 +131,3 @@ for i in range(3):
 #                 # also, look into adding documentation to Simulation (might be useful for above task too)
 
 #plt.close('all')
-
-axs[0][0].set_title('2D extrusion model',fontsize = 50)
-axs[0][1].set_title('Full 3D model',fontsize = 50)
-axs[0][0].set_ylabel('Rostral\nContact',fontsize = 50,rotation = 0,labelpad = 100)
-axs[1][0].set_ylabel('Center',fontsize = 50,rotation = 0,labelpad = 100)
-axs[2][0].set_ylabel('Caudal\nContact',fontsize = 50,rotation = 0,labelpad = 100)
-for ax in axs:
-    for a in ax:
-        a.axes.xaxis.set_visible(False)
-        a.spines['top'].set_visible(False)
-        a.spines['right'].set_visible(False)
-        a.spines['bottom'].set_visible(False)
-        a.spines['left'].set_visible(False)
-        a.get_yaxis().set_ticks([])
-plt.subplots_adjust(wspace = 0)
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([0.8, 0.15, 0.05, 0.7])
-
-mp = plt.cm.ScalarMappable()
-mp.set_clim(2.9,5.6)
-
-cb= fig.colorbar(
-    mappable=mp,
-    orientation='vertical',
-    aspect=20,
-    format='%0.2f',
-    cax = cbar_ax,
-)
-cb.ax.tick_params(labelsize = 50)
-cb.ax.tick_params(size = 15)
-cb.ax.tick_params(width = 5)
-
-cbar_ax.set_title(r'mA',fontsize = 50)
-colorbar_text_size_override = 100
-# colorbar font size
-# if colorbar_text_size_override is not None:
-#     cb.ax.tick_params(labelsize=colorbar_text_size_override if (
-#             colorbar_text_size_override is not None) else 25)
-# cb.update_ticks()
-fig.subplots_adjust(left=.15)
-fig.subplots_adjust(top=.95)
-fig.savefig('out/analysis/3dhm.png',dpi=500)
