8c0e050f3f60c2f8c7d2944a8b24152d3972b6b7
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-02-02 12:28:58 -0500

    dose response

--- examples/analysis/dose_response.py
+++ examples/analysis/dose_response.py
@@ -20,10 +20,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src.core.query import Query
 import pandas as pd
+import seaborn as sb
 
-threed = 673
+threed = 473
 
-samples = [670,672]
+samples = [470]
+
+samplename = '4R'
 
 models = [0]
 
@@ -62,5 +65,43 @@ q = Query({
 
 dat3d = q.threshdat3d(meanify = False)
 
+data = pd.concat([dat2d,dat3d])
+data.reset_index(inplace=True)
 
-# pd.concat(dats).to_csv('out/analysis/threshes.csv',index=False)
+# fig,axs = plt.subplots(ncols=5)
+def rename_var(df,di):
+    for variable,values in di.items():
+        for old,new in values.items():
+            df = df.replace(to_replace={variable:old},value=new)
+    return df
+
+redict = {
+  # "nsim":{
+  #     0:'(0) fiber diameter: 2\u03BCm',
+  #     1:'(1) fiber diameter: 5\u03BCm',
+  #     2:'(2) fiber diameter: 8\u03BCm',
+  #     3:'(3) fiber diameter: 11\u03BCm',
+  #     4:'(4) fiber diameter: 13\u03BCm'
+  #     },
+  "sample":{
+      samples[0]:'2D',
+      threed:'3D'
+      }
+}
+data = rename_var(data,redict)
+
+
+for i in range(5):
+    # ax = axs[i]
+    sb.color_palette("tab10")
+    # plt.figure()
+    plotdata = data[data.nsim==i]
+    plotdata = plotdata[plotdata['sample']!=670]
+    import seaborn as sb
+    sb.ecdfplot(data=plotdata,x='threshold',hue = 'sample')
+    plt.xscale('log')
+    plt.ylabel('Proportion of Fibers Activated')
+    plt.xlabel('Activation Threshold (mA, log scale)')
+    plt.title('Threshold eCDF for Sample {} (2D slice - highest r)'.format(samplename))
+plt.text(.05,-.25,'Note: fiber diameters (\u03bcm) from left to right: [2, 5, 8, 11, 13]',fontstyle='italic')
+plt.savefig(r'out/analysis/{}_ecdf.png'.format(threed),dpi=400,bbox_inches = 'tight')
