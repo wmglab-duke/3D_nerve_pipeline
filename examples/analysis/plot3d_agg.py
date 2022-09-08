0e2b0202a964d0fc31dead56ec3300518f6ed661
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-09-08 06:38:23 -0400

    enable plots

--- examples/analysis/plot3d_agg.py
+++ examples/analysis/plot3d_agg.py
@@ -73,8 +73,8 @@ def run_comparison(simdex, comparison):
 
 
 for simdex in config['sim_data'].keys():
-    # run_comparison(simdex, ['threshold', 'threshold3d'])
-    # run_comparison(simdex, ['threshold', 'peri_thk'])
-    # run_comparison(simdex, ['threshold3d', 'peri_thk'])
+    run_comparison(simdex, ['threshold', 'threshold3d'])
+    run_comparison(simdex, ['threshold', 'peri_thk'])
+    run_comparison(simdex, ['threshold3d', 'peri_thk'])
     run_comparison(simdex, ['threshold3d', 'peri_thk_act_site'])
-    # run_comparison(simdex, ['threshold3d', 'tortuosity3d'])
+    run_comparison(simdex, ['threshold3d', 'tortuosity3d'])
